6a8143bbcfb72bb348ec2c6bfe285901f2df4dee
Jakob Nacanaynay <63821647+j-nac@users.noreply.github.com>
2023-04-03 20:55:04 -0700

    Create base app

---
+++ app/src/App.jsx
@@ -0,0 +1,13 @@
+const TitleText = () => {
+    return <h1 className="text-title"><span className="text-dark-magenta">Rapid</span>ity</h1>
+}
+
+function App() {
+    return (
+        <div className="App bg-black text-white h-screen w-screen">
+            <TitleText />
+        </div>
+    );
+}
+
+export default App;
---
+++ app/tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      'black': '#212121',
+      'white': '#eeeeee',
+      'dark-magenta': '#a64d79',
+      'magenta': '#c27ba0ff',
+      'red': '#e06666ff',
+      'purple': '#8e7cc3ff',
+      'green': '#93c47dff',
+      'azure': '#6d9eebff',
+      'cyan': '#76a5afff',
+      'blue': '#6fa8dcff',
+      'orange': '#f6b26bff',
+      'yellow': '#ffd966ff',
+      'gray': '#595959',
+    },
+    fontFamily: {
+      sans: ['Fragment Mono', 'monospace'], // Tailwindcss uses sans by default
+      mono: ['Fragment Mono', 'monospace'],
+    },
+    fontSize: {
+      'title': '20vw',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
